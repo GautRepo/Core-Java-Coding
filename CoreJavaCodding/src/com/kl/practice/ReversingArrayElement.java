a2206902d745c75f4c53ea1dc81b35ab2b7de5cf
Dell <Dell@DESKTOP-7EO5D98>
2021-01-21 13:31:12 +0530

    Reversing Array element

---
+++ CoreJavaCodding/src/com/kl/practice/ReversingArrayElement.java
@@ -0,0 +1,31 @@
+package com.kl.practice;
+
+public class ReversingArrayElement {
+	public static void main(String[] args) {
+		int[] arr= {10,20,30,40};
+		int min=0;
+		int max=arr.length-1;
+		/*while(min<max) {
+			int dummy;
+			dummy=arr[min];
+			arr[min]=arr[max];
+			arr[max]=dummy;
+			min++;
+			max--;
+		}*/
+		
+		while(min<max) {
+			arr[min]+=arr[max];
+			arr[max]=arr[min]-arr[max];
+			arr[min]=arr[min]-arr[max];
+			min++;
+			max--;
+		}
+		for(int i : arr) {
+			System.out.print(i+" ");
+		}
+		
+		
+	}
+
+}
