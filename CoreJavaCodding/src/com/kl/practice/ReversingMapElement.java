80fb04f9379d0f49aa775fec06a1cc232bf26ba8
Dell <Dell@DESKTOP-7EO5D98>
2021-02-03 11:09:36 +0530

    sorting of hashMap element

---
+++ CoreJavaCodding/src/com/kl/practice/ReversingMapElement.java
@@ -0,0 +1,26 @@
+package com.kl.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ReversingMapElement {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("banana", "banana");
+		map.put("apple", "apple");
+		map.put("guava", "guava");
+		
+		for(Map.Entry<String, String> m : map.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+		System.out.println("________________________________________");
+		Map<String,String> treemap = new TreeMap<>(map);
+		
+		for(Map.Entry<String, String> m : treemap.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+	}
+
+}
