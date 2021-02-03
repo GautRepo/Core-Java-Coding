23ece1c46ecbcdc2c28a2c546ae0b38482e6b688
Dell <Dell@DESKTOP-7EO5D98>
2021-02-03 09:58:54 +0530

    reversing the list element

---
+++ CoreJavaCodding/src/com/kl/practice/ReversingTheList.java
@@ -0,0 +1,21 @@
+package com.kl.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReversingTheList {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		System.out.println(list);
+		Collections.reverse(list);
+		System.out.println(list);
+
+	}
+
+}
