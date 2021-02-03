e2c10b2464ec24a5650e0796fa626574fc6c5f49
Dell <Dell@DESKTOP-7EO5D98>
2021-02-03 15:24:48 +0530

    swapping list elements

---
+++ CoreJavaCodding/src/com/kl/practice/SwapingArrayListElement.java
@@ -0,0 +1,21 @@
+package com.kl.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SwapingArrayListElement {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		Collections.swap(list, 2, 3);
+		System.out.println(list);
+		
+
+	}
+
+}
