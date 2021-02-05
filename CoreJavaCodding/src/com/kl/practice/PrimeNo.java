6b8751eee0bb48e300cad3447fbfb9b8604c01cc
Dell <Dell@DESKTOP-7EO5D98>
2021-02-05 20:50:29 +0530

    prime number check

---
+++ CoreJavaCodding/src/com/kl/practice/PrimeNo.java
@@ -0,0 +1,25 @@
+package com.kl.practice;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a = sc.nextInt();
+		boolean flag=true;
+		for(int i=2; i<a;i++) {
+			if(a%i==0)
+				flag=false;
+			
+		}
+		
+		if(flag)
+			System.out.println("prime no");
+		else
+			System.out.println("not prime no");
+
+	}
+
+}
