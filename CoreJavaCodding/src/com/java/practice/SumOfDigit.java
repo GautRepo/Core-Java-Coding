8a357cb7dc49179738646bcb540958624df6f3a5
Dell <Dell@DESKTOP-7EO5D98>
2021-02-06 17:34:02 +0530

    Sum of digit of a number

---
+++ CoreJavaCodding/src/com/java/practice/SumOfDigit.java
@@ -0,0 +1,15 @@
+package com.java.practice;
+
+public class SumOfDigit {
+	public static void main(String[] args) {
+		int num = 45687;
+		int sum = 0;
+		while(num>0) {
+			sum +=num%10;
+			
+			num =num/10;
+		}
+		System.out.println(sum);
+	}
+
+}
