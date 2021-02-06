5f93321421bf13dd6855cfebcbde097d38518911
Dell <Dell@DESKTOP-7EO5D98>
2021-02-06 13:09:25 +0530

    factorial of number

---
+++ CoreJavaCodding/src/com/java/practice/FactersOfNumber.java
@@ -0,0 +1,16 @@
+package com.java.practice;
+
+public class FactersOfNumber {
+
+	public static void main(String[] args) {
+		int num = 77;
+		
+		for(int i =1; i<=num ; i++) {
+			if(num%i==0)
+				System.out.println("factorials of num are::"+i+" ");
+			
+		}
+
+	}
+
+}
