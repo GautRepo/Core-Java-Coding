299c0f90b6e51e1963df2266b13056c006aa4a13
Dell <Dell@DESKTOP-7EO5D98>
2021-02-08 14:02:20 +0530

    A to Z printing

---
+++ CoreJavaCodding/src/com/java/practice/AtoZPrinting.java
@@ -0,0 +1,13 @@
+package com.java.practice;
+
+public class AtoZPrinting {
+
+	public static void main(String[] args) {
+		
+		for(char i='a';i<='z';i++) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
