62f27174508fb7edf68377f6b5bacfd357b08a60
Dell <Dell@DESKTOP-7EO5D98>
2021-02-12 22:38:59 +0530

    Insertion sort

---
+++ DataStructuresAndAlgo/src/com/ud/algo/InsertionSort.java
@@ -0,0 +1,25 @@
+package com.ud.algo;
+
+import java.util.stream.Stream;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		Integer[] arr= {12,8,-15,20,4};
+		
+		for(int i=1;i<arr.length;i++ ) {
+			
+			int dummy =arr[i];
+			int j =0;
+			
+			for(j=i; j>0 && arr[j-1]>dummy; j--) {
+					arr[j]=arr[j-1];
+			}
+			arr[j]=dummy;
+		}
+		Stream<Integer> streams = Stream.of(arr);
+		streams.forEach(System.out::println);
+
+	}
+
+}
