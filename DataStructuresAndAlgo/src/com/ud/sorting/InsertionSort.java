81f34d1e089541dd304fa24c3879790371918c3e
Dell <Dell@DESKTOP-7EO5D98>
2021-02-15 16:39:15 +0530

    Insertion sort

--- DataStructuresAndAlgo/src/com/ud/sorting/InsertionSort.java
+++ DataStructuresAndAlgo/src/com/ud/sorting/InsertionSort.java
@@ -7,19 +7,20 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		Integer[] arr= {12,8,-15,20,-24};
 		
-		for(int i=1;i<arr.length;i++ ) {
-			
-			int dummy =arr[i];
-			int j =0;
-			
-			for(j=i; j>0 && arr[j-1]>dummy; j--) {
-					arr[j]=arr[j-1];
+		//Stream.of(arr).sorted((i1,i2)-> i1.compareTo(i2)).forEach(a->System.out.print(a+" "));
+		int temp;
+		
+		for(int i =1;i<arr.length;i++) {
+			temp =arr[i];
+			int j = i;
+			while(j>0 && arr[j-1]>temp) {
+				arr[j]=arr[j-1];
+				j--;
 			}
-			arr[j]=dummy;
+			arr[j]=temp;
+			
 		}
-		Stream<Integer> streams = Stream.of(arr);
-		streams.forEach(System.out::println);
-
+		Stream.of(arr).forEach(a->System.out.print(a+" "));
 	}
 
 }
